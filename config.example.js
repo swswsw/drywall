7f0253d4f87a0ff72bac03c3a9c9bc27ec72476c
sol wu <apiswswsw@gmail.com>
2014-07-19 12:22:12 -0700

    change project name

--- config.example.js
+++ config.example.js
@@ -4,8 +4,8 @@ exports.port = process.env.PORT || 3000;
 exports.mongodb = {
   uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost/drywall'
 };
-exports.companyName = 'Acme, Inc.';
-exports.projectName = 'Drywall';
+exports.companyName = 'CoinTipMe';
+exports.projectName = 'CoinTipMe';
 exports.systemEmail = 'your@email.addy';
 exports.cryptoKey = 'k3yb0ardc4t';
 exports.loginAttempts = {
